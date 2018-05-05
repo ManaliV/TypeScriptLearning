80684c4e1e3832d01abaf8fad1eca6bf02612af8
ManaliV <38902463+ManaliV@users.noreply.github.com>
2018-05-05 11:32:57 +0530

    Update TypeScript1.ts

--- TypeScript1.ts
+++ TypeScript1.ts
@@ -4,4 +4,23 @@ function add(val1:number, val2:number):number
 {
  return val1 + val2;  
 }
-var result = add(10, 20);|
+var result = add(10, 20);
+
+type myType = number|string;
+function add2(val1:myType, val2:myType):myType
+{
+  if(typeof val1 === 'number' && typeof val2 === 'number')
+  {
+   return val1 + val2;   
+  }
+  if(type of val1 === 'string' && type of val2 === 'string')
+  {
+   return val1 + val2;   
+  }
+ 
+  return null;
+  
+ 
+}
+
+var result2 = add2("test1", 'test2");
